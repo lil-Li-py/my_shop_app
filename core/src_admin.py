154c1e5425fb3dd42b3aaab95deeb2ffaa96bc2d
unknown <1563410554@qq.com>
2024-05-13 13:13:32 +0800

    管理员窗口视图层

--- core/src_admin.py
+++ core/src_admin.py
@@ -7,6 +7,7 @@ from functools import partial
 from PyQt6 import QtCore
 from PyQt6.QtWidgets import QWidget, QTableWidgetItem, QPushButton, QMessageBox, QTextEdit, QFrame, \
     QHBoxLayout, QInputDialog
+
 from db.ui.admin import Ui_Form as AdminMixin
 from db.ui.items import Ui_Form as ItemsMixin
 from db.db_handler import admin
@@ -15,38 +16,56 @@ from interface.customer import call_customer
 _translate = QtCore.QCoreApplication.translate
 
 
+# 管理员界面
 class AdminUi(AdminMixin, QWidget):
     def __init__(self, start):
+        """
+        :type start: 开始界面(来自core.src_start StartUi
+        """
         super(AdminUi, self).__init__()
+        self._sub_win1 = None  # 管理用户窗口
+        self._sub_win2 = None  # 管理商家窗口
+        self._sub_win3 = None  # 商品上架审核窗口
         self.start = start
-        self._win = None
         self.setupUi(self)
         self.setWindowTitle('管理员')
 
+    # 检索所有用户
     def check_all_customers(self):
         info = admin(0)
         column = ['用户名', '']
-        self._win = ManageWindow(info, column)
-        self._win.show()
+        self._sub_win1 = ManageWindow(info, column)
+        self._sub_win1.setWindowTitle('所有用户')
+        self._sub_win1.show()
 
+    # 检索所有商家
     def check_all_shopkeepers(self):
         info = admin(1)
         column = ['商家名', '']
-        self._win = ManageWindow(info, column)
-        self._win.show()
+        self._sub_win2 = ManageWindow(info, column)
+        self._sub_win2.setWindowTitle('所有商家')
+        self._sub_win2.show()
 
+    # 检索所有待审核的商品
     def verify_listing_items(self):
         info = admin(2)
         column = ['商品名', '价格', '商家', '类型', '商品介绍', '提交时间', '']
-        self._win = ManageWindow(info, column)
-        self._win.show()
+        self._sub_win3 = ManageWindow(info, column)
+        self._sub_win3.setWindowTitle('所有待审核商品')
+        self._sub_win3.show()
 
+    # 重写关闭方法
     def closeEvent(self, event):
         event.accept()
+        # 获取所有子窗口, 若打开则关闭
+        for i in [self._sub_win1, self._sub_win2, self._sub_win3]:
+            if i:
+                i.close()
         time.sleep(1)
         self.start.show()
 
 
+# 管理窗口(可同时打开上述三个窗口)
 class ManageWindow(ItemsMixin, QWidget):
     def __init__(self, info, column):
         super(ManageWindow, self).__init__()
@@ -55,8 +74,10 @@ class ManageWindow(ItemsMixin, QWidget):
         self.setupUi(self)
         self._insert_items()
 
+    # tablewidget插入值
     def _insert_items(self):
-        self.tableWidget.setColumnCount(len(self._column))
+        self.tableWidget.setColumnCount(len(self._column))  # 设置列
+        # 添加表头
         for i in range(self.tableWidget.columnCount()):
             item = QTableWidgetItem()
             self.tableWidget.setHorizontalHeaderItem(i, item)
@@ -65,11 +86,12 @@ class ManageWindow(ItemsMixin, QWidget):
         row = len(self._info)
         if not row:
             return
-        self.tableWidget.setRowCount(row)
+        self.tableWidget.setRowCount(row)  # 设置行
+        # 为每行每列的每个item添加值
         for i in range(self.tableWidget.rowCount()):
             for j in range(self.tableWidget.columnCount()):
                 item = QTableWidgetItem()
-                item.setTextAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+                item.setTextAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)  # 设置字体居中
                 # 查看用户与商家窗口
                 if self.tableWidget.columnCount() == 2:
                     if not j:
@@ -83,8 +105,10 @@ class ManageWindow(ItemsMixin, QWidget):
                 # 审核上架商品窗口
                 else:
                     if j == 5:
+                        # 给提交时间添加tooltip
                         item.setToolTip(_translate("Form", f"{self._info[i][j]}"))
                     if j == 4:
+                        # 创建textedit以存储商品简介以更好的展示
                         textedit = QTextEdit()
                         textedit.setPlainText(_translate("Form", f"{self._info[i][j]}"))
                         textedit.setEnabled(False)
@@ -92,6 +116,7 @@ class ManageWindow(ItemsMixin, QWidget):
                     elif j != 6:
                         item.setText(_translate("Form", f"{self._info[i][j]}"))
                     else:
+                        # 设置审核答复按钮
                         frame = QFrame()
                         button1 = QPushButton(frame)
                         button1.setText(_translate("Form", "接受"))
@@ -110,21 +135,26 @@ class ManageWindow(ItemsMixin, QWidget):
                         self.tableWidget.setCellWidget(i, j, frame)
                     self.tableWidget.setItem(i, j, item)
 
+    # 用户/商家的冻结/解冻方法
     def _do_frozen(self, item):
         row = self.tableWidget.row(item)
+        # 修改解冻或冻结
         is_frozen = 0 if self._info[row][1] else 1
         call_customer(self._info[row][0], update=True, is_frozen=is_frozen)
         QMessageBox.information(self, '提示', '修改成功')
         self.close()
 
+    # 审核通过
     def _confirm(self, item):
         row = self.tableWidget.row(item)
         item_name = self.tableWidget.item(row, 0).text()
         shop_name = self.tableWidget.item(row, 2).text()
+        # 为商家的listing_items添加审核记录 0/1 表示通过或不通过, 后面为理由(主要是不通过的)
         appends = [1, '']
         admin(3, appends=appends, item_name=item_name, shop_name=shop_name)
         self.tableWidget.removeRow(row)
 
+    # 审核拒绝
     def _reject(self, item):
         row = self.tableWidget.row(item)
         item_name = self.tableWidget.item(row, 0).text()
@@ -137,6 +167,7 @@ class ManageWindow(ItemsMixin, QWidget):
         self.tableWidget.removeRow(row)
 
 
+# 初始化启动
 def run(start):
     start.admin = AdminUi(start)
     start.admin.show()
