48f03a41b757b9d212d1639ed581406021d45cc8
unknown <1563410554@qq.com>
2024-05-20 12:41:10 +0800

    公共方法

--- lib/common.py
+++ lib/common.py
@@ -4,22 +4,26 @@ import logging
 import conf.settings
 
 
-# 密码加密储存
 def encrypt(pwd: str) -> str:
+    """
+    密码加密
+    :return: 返回加密后的密码
+    """
     return hashlib.sha256(pwd.encode()).hexdigest()
 
 
-# qt报错设置
 def except_hook(cls, exception, traceback):
+    """
+    Qt文件报错设置
+    """
     sys.__excepthook__(cls, exception, traceback)
 
 
-# 日志记录
 def logging_save(mode: int, content: str) -> None:
     """
+    日志记录
     :param mode: 0 for customer, 1 for shopkeeper
-    :param content:
-    :return: nothing
+    :param content: 拒绝原因|...
     """
     dic = {0: 'customer', 1: 'shopkeeper', 2: 'item'}
     _logging = logging.getLogger(dic[mode])
